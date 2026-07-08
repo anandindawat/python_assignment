bd56a14adfaabd15c7e8917e59d7b661c9305ff9
anandindawat <anandindawat9@gmail.com>
2026-07-08 22:51:34 +0530

    Create python_session.py

---
+++ python_session.py
@@ -0,0 +1,22 @@
+"""This code store item price and print them 
+and check the order is eligible for free delivery """
+item_price = 250
+delivery_fee = 30
+coupon_discount = 20
+# in this section print variables
+print(item_price)
+print(delivery_fee)
+print(coupon_discount)
+# in this section print if item price is greater than or equal to 1000              
+if item_price >= 1000:
+    print("Eligible for free delivery")
+else:
+    print("Delivery charges apply")
+
+team_name = "Rajasthan Royals"
+runs_scored  = 210
+overs_played = 20.0
+
+print(team_name)
+print(runs_scored)
+print(overs_played)
